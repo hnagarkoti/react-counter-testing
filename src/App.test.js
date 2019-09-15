4f5598d94c9de20a8034d045e785f07bdf56730f
Hemant Nagarkoti <hemant.nagarkoti@walmartlabs.com>
2019-09-15 14:20:33 +0530

    test page added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders without crashing', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+})
+
+test("render increment button", () => {});
+
+test("renders counter display", () => {});
+
+test("counter starts with 0", () => {});
+
+test('clicking btn increment counter btn display', () => {})
